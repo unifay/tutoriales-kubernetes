a66ab4811cb148af86090ac8c0977eb620b31ca5
aapiro <tutorials@unifay.com>
2020-10-30 01:15:29 -0500

    resto de la instalacion

--- enableIpTables.sh
+++ enableIpTables.sh
@@ -1,5 +1,24 @@
-echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+#En el nodo master
 
-sudo modprobe br_netfilter
+#Inicializar el cluster:
 
-sudo sysctl -p
+sudo nano /proc/sys/net/ipv4/ip_forward
+(Change from 0 to 1) 
+
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+# configuracion de kubeconfig:
+
+mkdir -p $HOME/.kube
+
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#Instalacion de Flannel:
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/3f7d3e6c24f641e7ff557ebcea1136fdf4b1b6a1/Documentation/kube-flannel.yml
+
+#En cada nodo de kubernetes
+sudo kubeadm join $controller_private_ip:6443 --token $token --discovery-token-ca-cert-hash $hash
+
+kubectl get nodes
